0c6aa01ead1a15a90d03ac8c7ce50f5c0ef62d2c
Raj Aryan <rajaryan.rsr@gmail.com>
2022-12-27 23:17:27 +0530

    Exception class added

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,6 @@ child:const MyApp()));
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
